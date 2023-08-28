f7230ab9ee1f37650dc8bbdaf9dca0bfdaa16292
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2023-08-27 22:05:46 -0600

    🐛 Fix graphical glitch when content is smaller than notch width

--- Sources/DynamicNotchKit/NotchView.swift
+++ Sources/DynamicNotchKit/NotchView.swift
@@ -17,7 +17,9 @@ struct NotchView: View {
 
                 VStack(spacing: 0) {
                     Spacer()
-                        .frame(width: self.dynamicNotch.notchWidth, height: self.dynamicNotch.notchHeight)
+                        .frame(width: self.dynamicNotch.notchWidth + 20, height: self.dynamicNotch.notchHeight)
+                        // We add an extra 20 here because the corner radius of the top increases when shown. 
+                        // (the remaining 10 has already been accounted for)
 
                     self.dynamicNotch.content
                         .blur(radius: self.dynamicNotch.isVisible ? 0 : 10)
@@ -35,7 +37,7 @@ struct NotchView: View {
                     GeometryReader { _ in   // This helps with positioning everything
                         HStack {
                             Spacer(minLength: 0)
-                            NotchShape(cornerRadius: self.dynamicNotch.isVisible ? 20 : 10)
+                            NotchShape(cornerRadius: self.dynamicNotch.isVisible ? 20 : nil)
                                 .frame(
                                     width: self.dynamicNotch.isVisible ? nil : self.dynamicNotch.notchWidth,
                                     height: self.dynamicNotch.isVisible ? nil : self.dynamicNotch.notchHeight
